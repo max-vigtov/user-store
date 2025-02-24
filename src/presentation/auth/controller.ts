72250ec04c737b5764417303585f3cc8f9db6f42
Max Vigueras <mvt.2000@hotmail.com>
2025-02-24 15:25:51 -0600

    Routes & Database: Auth routes & Mongo connection added.

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response  } from "express";
+
+
+export class AuthController {
+
+	constructor() {}
+
+	registerUser = ( req: Request, res: Response ) => {
+		res.json('registerUser')
+	}
+
+	loginUser = ( req: Request, res: Response ) => {
+		res.json('loginUser')
+	}
+
+	validateEmail = ( req: Request, res: Response ) => {
+		res.json('validateEmail')
+	}	
+
+}
